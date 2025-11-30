b3a0b4b90a61332fecd4df9af16a8d570052bd56
qicosmos <qicosmos@163.com>
2025-11-30 19:27:18 +0800

    login html

--- feather.cpp
+++ feather.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
+#include <cinatra.hpp>
+using namespace cinatra;
 
-int main() {}
+int main() {
+  coro_http_server server(std::thread::hardware_concurrency(), 9000);
+  server.set_static_res_dir("", "html");
+  server.sync_start();
+}
